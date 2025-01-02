a173f78e9691c3ba325f60592ccbdc678105253a
Shafin Ashraf <shafin.ashraf@monstar-lab.com>
2025-01-02 11:52:52 +0600

    update flask start point

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def upload_file():
     return jsonify({'error': 'File type not allowed'}), 400
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
